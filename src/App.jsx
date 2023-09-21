1e61e3cab0aa1abdcb6e06818ff1308f6dbb1fb3
Axel Cymerman <axelcy@gmail.com>
2023-09-21 01:27:32 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<Layout />}>
+    //       <Route index element={<Home/>} />
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <Home />
   )
 }
 
