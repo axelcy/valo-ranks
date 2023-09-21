405ac801128c448283d6151ffdbb567ec3d1bdaf
Axel Cymerman <axelcy@gmail.com>
2023-09-21 19:05:09 -0300

    v1.33

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import './styles/Home.css'
 import defaultAccounts from "../mocks/accounts"
 
 function Home() {
-    const APP_VERSION = '1.32'
+    const APP_VERSION = '1.33'
     const [accounts, setAccountsState] = useState(JSON.parse(localStorage.getItem('accounts')) || [])
 
     const setAccounts = newAccounts => {
@@ -30,8 +30,10 @@ function Home() {
                 <button onClick={resetAccounts}>Borrar todas las cuentas</button>
                 <button onClick={setChinchulines}>Poner a los chinchulines</button>
             </header>
-            <h1 className="main-title no-select">Chinchu-Ranks {APP_VERSION}</h1>
-            <form className="flex justify-center" onSubmit={handleSubmit}>
+            <div className="title-container">
+                <h1 className="main-title no-select">Chinchu-Ranks</h1><h3 className="nametag">v{APP_VERSION}</h3>
+            </div>
+            <form className="flex justify-center pb-10" onSubmit={handleSubmit}>
                 <input autoComplete="off" className="search-bar no-select w-1/3 rounded-full px-5 py-3" name="account" type="text" placeholder="CLG Manzana Roja#vsc" />
             </form>
             <section className="account-list no-select">
