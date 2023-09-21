b37f16f1ab599818a02507b31ffaacb6f229ab9e
Axel Cymerman <axelcy@gmail.com>
2023-09-21 01:16:15 -0300

    v2 lista

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -13,34 +13,51 @@ function Account({ name }) {
     const rankTitle = useRef(null)
     
     useEffect(() => async () => {
-        const _account = await new ValorantAPI(name).account()
-        setAccount(_account)
-        const _mmr = await new ValorantAPI(name).mmr()
-        setMmr(_mmr)
-        // filter: contrast(0.5);
-        firstSection.current.style.backgroundImage = `url(${_account.card.wide})`
-        rankTitle.current.style.color = tierColors[_mmr?.currenttierpatched]  
-        progress.current.style.width = `${_mmr?.ranking_in_tier}%`
+        try {
+            const _account = await new ValorantAPI(name).account()
+            setAccount(_account)
+            const _mmr = await new ValorantAPI(name).mmr()
+            setMmr(_mmr)
+            // filter: contrast(0.5);
+            firstSection.current.style.backgroundImage = `url(${_account.card.wide})`
+            rankTitle.current.style.color = tierColors[_mmr?.currenttierpatched]  
+            progress.current.style.width = `${_mmr?.ranking_in_tier}%`
+        }
+        catch (err) {
+            //
+        }
     }, [name])
 
     return (
         <article>
-            <section className="first-section" ref={firstSection}>
-                <h3><a target="_blank" href={trackerUrl} draggable={false}>{account?.name}<span className="nametag">#{account?.tag}</span></a></h3>
-            </section>
-            <section className="second-section">
+            <div>
                 <img src={mmr?.images?.large} className="rank-image" draggable={false} />
-                <div className="rank-title" ref={rankTitle}>{tierTranslations[mmr?.currenttierpatched]}</div>
-                <div className="progress-container">
-                    <div className="rank-progress-bar">
-                        <div ref={progress} className="rank-progress"></div>
+                {
+                    mmr?.mmr_change_to_last_game > 0 ? 
+                    <div className="last-mmr" ><span className="mmr-green">+{mmr.mmr_change_to_last_game}</span></div> : // hacer modulo
+                    <div className="last-mmr" ><span className="mmr-red">-{Math.abs(mmr.mmr_change_to_last_game)}</span></div>
+                }
+                
+            </div>
+            <div className="inner-container">
+                <h3><a target="_blank" href={trackerUrl} draggable={false}>
+                    {account?.name}<span className="nametag">#{account?.tag}</span></a>
+                </h3>
+                <section className="first-section" ref={firstSection}>
+                    <div className="rank-title" ref={rankTitle}>{tierTranslations[mmr?.currenttierpatched]}</div>
+                </section>
+                <section className="second-section">
+                    <div className="progress-container">
+                        <div className="rank-progress-bar">
+                            <div ref={progress} className="rank-progress"></div>
+                        </div>
                     </div>
-                </div>
-                <div className="rank-rating-container">
-                    <p className="rank-rating-text">RANK RATING</p>
-                    <p className="rank-rating"><span className="actual-rating">{mmr?.ranking_in_tier ?? '0'}</span> / 100</p>
-                </div>
-            </section>
+                    <div className="rank-rating-container">
+                        <p className="rank-rating-text">RANK RATING</p>
+                        <p className="rank-rating"><span className="actual-rating">{mmr?.ranking_in_tier ?? '0'}</span> / 100</p>
+                    </div>
+                </section>
+            </div>
         </article>
     )
 }
