9ba8c72f27d550f3acea81637c207062a098c23c
Axel Cymerman <axelcy@gmail.com>
2023-09-21 01:30:15 -0300

    Update useTrackerUrl.js

--- src/hooks/useTrackerUrl.js
+++ src/hooks/useTrackerUrl.js
@@ -1,5 +1,6 @@
 // https://tracker.gg/valorant/profile/riot/CLG%20Manzana%20Roja%23vsc/overview
-const TRACKER_URL = import.meta.env.VITE_TRACKER_URL
+// const TRACKER_URL = import.meta.env.VITE_TRACKER_URL
+const TRACKER_URL = 'https://tracker.gg/valorant/profile/riot'
 export default function useTrackerUrl(name = '') {
     return `${TRACKER_URL}/${name.replace(/ /g, '%20').replace(/#/g, '%23')}/overview`
 }
