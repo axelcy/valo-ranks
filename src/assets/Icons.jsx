d6552e9ee954a47855f843556646699a4e27ae96
Gabriel Guivisdalsky <axelcy@gmail.com>
2023-10-04 09:01:23 -0300

    v1.6.5

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -24,3 +24,25 @@ export const NameCopied = props => (
         <path d="M9 14l2 2l4 -4"></path>
     </svg>
 )
+
+
+export const SadFace = props => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" className="sad-face-icon icon icon-tabler icon-tabler-mood-sad" width={24} height={24} viewBox="0 0 24 24" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+        <path d="M9 10l.01 0"></path>
+        <path d="M15 10l.01 0"></path>
+        <path d="M9.5 15.25a3.5 3.5 0 0 1 5 0"></path>
+    </svg>
+)
+
+export const TrashCan = props => (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" className="trash-can icon icon-tabler icon-tabler-trash" width={24} height={24} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M4 7l16 0"></path>
+        <path d="M10 11l0 6"></path>
+        <path d="M14 11l0 6"></path>
+        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12"></path>
+        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3"></path>
+    </svg>
+)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@ import { useState } from "react"
 import Account from "../components/Account"
 import './styles/Home.css'
 import mappedAccounts from "../mocks/accounts"
-import WavesBackground from "../components/WavesBackground"
+// import WavesBackground from "../components/WavesBackground"
+import { SadFace, TrashCan } from "../assets/Icons"
 
 // const APP_VERSION = '1.6.3'
 
@@ -22,6 +23,7 @@ function Home() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        e.target.blur()
         setAccounts([...accounts, e.target.account.value])
         e.target.account.value = ''
     }
@@ -36,15 +38,22 @@ function Home() {
                 <h1 className="main-title no-select">Chinchu Ranks</h1>
                 <h3 className="nametag no-select">v{import.meta.env.VITE_APP_VERION}</h3>
             </div>
-            <form className="flex justify-center pb-10" onSubmit={handleSubmit}>
+            <form className="main-form flex justify-center pb-10 gap-5 items-center" onSubmit={handleSubmit}>
+                <TrashCan onClick={resetAccounts} />
                 <input autoComplete="off" className="search-bar no-select w-1/3 rounded-full px-5 py-3" name="account" type="text" placeholder="CLG Manzana Roja#vsc" />
             </form>
             <section className="account-list no-select">
-                {accounts?.map((account, index) => (
-                    <Account key={index} index={index} name={account} closeAccount={() => closeAccount(index, account)} />
-                ))}
+                {
+                    accounts.length > 0 ? accounts.map((account, index) => (
+                        <Account key={index} index={index} name={account} closeAccount={() => closeAccount(index, account)} />
+                    )) :
+                    <div className="no-accounts-container">
+                        <SadFace />
+                        <p className="no-accounts-msg">Parece que no tienes cuentas... Prueba <span onClick={setMappedAccounts}>agregar algunas!</span></p>
+                    </div>
+                }
             </section>
-            <WavesBackground />
+            {/* <WavesBackground /> */}
         </main>
     )
 }
