245480854b40c2571fa27af16ef47eab63eb2f48
Axel Cymerman <88910313+axelcy@users.noreply.github.com>
2023-10-16 01:20:42 -0300

    Actualizar accounts.js

--- src/mocks/accounts.js
+++ src/mocks/accounts.js
@@ -4,7 +4,7 @@ const defaultAccounts = [
     // 'CLG TeiToTeo84#1815',
     'CLG Santik2010#TRUJO',
     // 'LAC Domix#640',
-    'Dere Croissant#BRL1',
+    // 'Dere#BRL1',
 ]
 
 export default defaultAccounts
