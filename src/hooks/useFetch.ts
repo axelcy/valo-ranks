f9d83bbb6f0ef93c4ebe0df7c0422c1b94315863
Axel Cymerman <axelcy@gmail.com>
2024-02-04 05:13:32 -0300

    api key

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,6 +1,10 @@
 export default async function useFetch(endpoint: string): Promise<any> {
     try {
-        const response = await fetch(import.meta.env.VITE_API_URL + endpoint)
+        const response = await fetch(import.meta.env.VITE_API_URL + endpoint, {
+            headers: {
+                Authorization: import.meta.env.VITE_API_KEY
+            }
+        })
         // if (!response.ok) throw new Error('Valorant API error :(')
         if (response?.status === 429) throw new Error('EXPLOTACIÓN DE LA API MUCHAS LLAMADAS :V')
         return (await response.json()).data
